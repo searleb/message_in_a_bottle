842ca4b414933f3c20e485e0d5490a8ae5ecea54
bill <billsearle@me.com>
2014-08-18 23:50:26 +1000

    removed test modals. Installed vex.

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,47 +1,63 @@
+var blobs = blobs || {};
+
 $(document).ready(function() {
 
-	var displayBlob = function () {
+	blobs.makeBlobs = function () {
 		$.ajax({
 			url: '/ajax/lookup',
 			type: 'get',
 			dataType: 'json',
 		}).done(function(response){
 			for (var i = response.message.length - 1; i >= 0; i--) {
-				var messageId = response.message[i].id
-				var messageUrl = '#modal' + messageId 
+				blobs.messageId = response.message[i].id;
+				blobs.messageUrl = '/messages/' + blobs.messageId 
+				blobs.createdAt = response.message[i].created_at;
+				blobs.userId = response.message[i].user_id;
+				blobs.userMessage = response.message[i].message;
 				
-				var newBlobModal = $("<div />", {
-					class : 'remodal',
-					'data-remodal-id' : "#modal"+messageId,
-					text : messageId
-				}).css({
-					'display' : 'hidden'
-				}).appendTo('html');
+				console.log( blobs.messageId +" "+ blobs.createdAt +" "+ blobs.userMessage +" "+ blobs.userId);
+				
+				
+				// var newBlobModal = $("<div />", {
+				// 	class : 'remodal',
+				// 	'data-remodal-id' : "#modal"+messageId,
+				// 	text : messageId
+				// }).css({
+				// 	'display' : 'hidden'
+				// }).appendTo('html');
 
-					var newBlobLink = $("<a />", {
-					    href : messageUrl,
-					    text : messageId,
-					    class : ('layer', 'remodal-bg'),
-					    'data-depth' : '0.40'
+					var blobDiv = $("<div />", {
+					    class : ('blob')
 					  });
+					var blobAhref = $("<a />", {
+							href : blobs.messageUrl,
+							text : blobs.messageId
+					}).appendTo(blobDiv);
 
 					var divsize = 50
 	    		var posx = (Math.random() * ($(document).width() - divsize)).toFixed();
 	    		var posy = (Math.random() * ($(document).height() - divsize)).toFixed();
 
-	    		newBlobLink.css({
+	    		blobDiv.css({
 	        	'position':'absolute',
 	        	'left':posx+'px',
 	        	'top':posy+'px',
 	        	// 'display':'none'
-	    		}).appendTo( '#container' ).fadeIn(100);
+	    		}).appendTo( '#messages-container' ).fadeIn(100);
 			};
 		});
 	}
-displayBlob();
-});
+			
+		blobs.modal = function(){
+			vex.dialog.open({
+  		message: "You have found a message from " + blobs.userId + "." + '<br>' + "The message reads: " + "'" + blobs.userMessage + "'" + "<br>" + "Dated: " + blobs.createdAt,
+  		callback: function(value) {
+    	return console.log(value ? 'Successfully destroyed the planet.' : 'Chicken.');
+  			}
+			});
+		}; // end modal
 
+	blobs.makeBlobs();
 
-// $(document).ready(function(){
-// 	$('#scene').parallax();
-// })
+}); // end document ready
+
