5590af566e7008e50c2d975935de5d80a253e6a8
bill <billsearle@me.com>
2014-08-19 01:37:28 +1000

    removed email create record

--- app/models/concerns/active_record_helpers.rb
+++ app/models/concerns/active_record_helpers.rb
@@ -7,7 +7,7 @@
     module ClassMethods
       def find_for_oauth(auth)
         record = where(provider: auth.provider, uid: auth.uid.to_s).first
-        record || create(provider: auth.provider, nickname: auth.info.nickname, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0,20])
+        record || create(provider: auth.provider, nickname: auth.info.nickname, uid: auth.uid, password: Devise.friendly_token[0,20])
       end
     end
   end
