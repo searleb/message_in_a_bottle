ceb40b1ebca4695870bdc1fe3eac06e80c99ca5f
Bill Searle <billsearle@me.com>
2014-09-09 16:36:35 +1000

    Update createBlobs.js

--- app/assets/javascripts/createBlobs.js
+++ app/assets/javascripts/createBlobs.js
@@ -1,11 +1,16 @@
+// set up global blobs object
 var blobs = blobs || {};
+
+// counter used to loop animation
 var idCounter = 1
 
 blobs.animate = function(counterID){
+	// define size of blob
 	var divsize = 60
+	// generate random position within the current window size
 	var posx = (Math.random() * ($(window).width() - divsize)).toFixed();
 	var posy = (Math.random() * ($(window).height() - divsize)).toFixed();
-	
+	// animate each blob to its position over a second time span
 	$('#blob'+counterID).velocity({
 		'left' : posx+'px',
 		'top' : posy+'px',
@@ -15,15 +20,16 @@ blobs.animate = function(counterID){
 };
 
 $(document).ready(function() {
-
+	
 	blobs.makeBlobs = function () {
+		// make a get request for the a sample of the database
 		$.ajax({
 			url: '/messages/sample',
 			type: 'get',
 			dataType: 'json',
 		}).done(function(response){
 			blobs.response = response
-
+			// for each response object create a blob
 			for (var i = response.length - 1; i >= 0; i--) {
 				
 				var messageId = response[i].id;
@@ -34,7 +40,7 @@ $(document).ready(function() {
 				var userNickName = response[i].user.nickname;
 				var userImage = response[i].user.image;
 				idCounter ++;
-
+				// randomly generate a depth value for paralllax.js
 				var dataDepth = (Math.random() * (1 - 0.20) + 0).toFixed(2);
 				// Create and append all blobs to #scene
 				var blobDiv = $("<li />", {
@@ -54,7 +60,7 @@ $(document).ready(function() {
 				var blobText = $("<p />", {
 					text : userNickName,
 				}).appendTo( '#blob'+idCounter );
-
+				// call the animate function to animate each blob one by one as they are created	
 				(function () {
 					var c = idCounter;
 					setTimeout(function () {blobs.animate(c) }, Math.random() * 3000);
@@ -72,12 +78,12 @@ $(document).ready(function() {
 				$scene.parallax('calibrate', false, true);
 			});
 }	
-
+// new search button, clears the page and reruns makeBlobs
 $('#reset').on('click', function(){
 		$('#scene').empty();	
 		blobs.makeBlobs();
 }); 
-
+// start on document.ready
 blobs.makeBlobs();
 }); // end document ready
 
